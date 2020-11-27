c0164d790d1b44bf671f459b8425d5af92ab3651
riku <riku.suny@gmail.com>
2020-11-27 18:10:04 +0900

    add button to TakeFlash for taking a video

--- rn/rn_app/ios/rn_app/AppDelegate.m
+++ rn/rn_app/ios/rn_app/AppDelegate.m
@@ -1,8 +1,6 @@
 #import "AppDelegate.h"
 #import "RNLine-Swift.h"
 
-
-
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
--- rn/rn_app/src/components/flashs/TakeFlash.tsx
+++ rn/rn_app/src/components/flashs/TakeFlash.tsx
@@ -6,6 +6,7 @@ import {
   Dimensions,
   LayoutAnimation,
   ViewStyle,
+  Image,
 } from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import {Button} from 'react-native-elements';
@@ -15,9 +16,10 @@ import FIcon from 'react-native-vector-icons/FontAwesome';
 type Props = {goBack: () => void};
 
 export const TakeFlash = ({goBack}: Props) => {
-  const [recordingVideo, setRecordingVideo] = useState(false);
   const [backPhotoMode, setBackPhotoMode] = useState(true);
-  const [isShooted, setIsShooted] = useState(false);
+  const [takenPhoto, setTakenPhoto] = useState<string | null>(null);
+  const [takenVideo, setTakenVideo] = useState<string | null>(null);
+  const [recordingVideo, setRecordingVideo] = useState(false);
 
   const shootButtonFlexstyle: ViewStyle = {
     justifyContent: !recordingVideo ? 'space-evenly' : 'center',
@@ -29,6 +31,7 @@ export const TakeFlash = ({goBack}: Props) => {
     if (cameraRef.current) {
       const options = {quality: 0.9, base64: true};
       const data = await cameraRef.current.takePictureAsync(options);
+      setTakenPhoto('data:image/jpeg;base64,' + data.base64);
       console.log(data);
     }
   };
@@ -37,64 +40,97 @@ export const TakeFlash = ({goBack}: Props) => {
     if (cameraRef.current) {
       const options = {quality: RNCamera.Constants.VideoQuality['1080p']};
       const data = await cameraRef.current.recordAsync(options);
+      console.log(data);
     }
   };
+
+  const stopVideo = () => {
+    if (cameraRef.current) {
+      cameraRef.current.stopRecording();
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <RNCamera
-        ref={cameraRef}
-        style={styles.preview}
-        type={
-          backPhotoMode
-            ? RNCamera.Constants.Type.back
-            : RNCamera.Constants.Type.front
-        }
-      />
-      <Button
-        icon={
-          <MIcon name={'chevron-right'} style={{color: 'white'}} size={35} />
-        }
-        containerStyle={styles.backButtonContainer}
-        buttonStyle={styles.backButton}
-        onPress={goBack}
-      />
-      <Button
-        icon={<MIcon name="party-mode" style={{color: 'white'}} size={25} />}
-        containerStyle={styles.changePhotoModeButtonContainer}
-        buttonStyle={styles.changePhotoModeButton}
-        onPress={() => {
-          backPhotoMode ? setBackPhotoMode(false) : setBackPhotoMode(true);
-        }}
-      />
-      <View style={{...styles.shootButtonBox, ...shootButtonFlexstyle}}>
-        {!recordingVideo && (
+      {!takenPhoto && !takenVideo ? (
+        <>
+          <RNCamera
+            ref={cameraRef}
+            style={styles.preview}
+            type={
+              backPhotoMode
+                ? RNCamera.Constants.Type.back
+                : RNCamera.Constants.Type.front
+            }
+          />
           <Button
             icon={
               <MIcon
-                name="enhance-photo-translate"
-                size={20}
+                name={'chevron-right'}
                 style={{color: 'white'}}
+                size={35}
               />
             }
-            buttonStyle={styles.shootButton}
-            onPress={takePicture}
+            containerStyle={styles.backButtonContainer}
+            buttonStyle={styles.backButton}
+            onPress={goBack}
           />
-        )}
-        <Button
-          icon={
-            recordingVideo ? (
-              <FIcon name="square" style={{color: 'red'}} size={25} />
-            ) : (
-              <MIcon name="video-call" size={20} style={{color: 'white'}} />
-            )
-          }
-          buttonStyle={styles.shootButton}
-          onPress={() => {
-            LayoutAnimation.easeInEaseOut();
-            setRecordingVideo(true);
-          }}
-        />
-      </View>
+          <Button
+            icon={
+              <MIcon name="party-mode" style={{color: 'white'}} size={25} />
+            }
+            containerStyle={styles.changePhotoModeButtonContainer}
+            buttonStyle={styles.changePhotoModeButton}
+            onPress={() => {
+              backPhotoMode ? setBackPhotoMode(false) : setBackPhotoMode(true);
+            }}
+          />
+          <View style={{...styles.shootButtonBox, ...shootButtonFlexstyle}}>
+            {!recordingVideo && (
+              <Button
+                icon={
+                  <MIcon
+                    name="enhance-photo-translate"
+                    size={20}
+                    style={{color: 'white'}}
+                  />
+                }
+                buttonStyle={styles.shootButton}
+                onPress={takePicture}
+              />
+            )}
+            <Button
+              icon={
+                recordingVideo ? (
+                  <FIcon name="square" style={{color: 'red'}} size={25} />
+                ) : (
+                  <MIcon name="video-call" size={20} style={{color: 'white'}} />
+                )
+              }
+              buttonStyle={styles.shootButton}
+              onPress={() => {
+                if (!recordingVideo) {
+                  LayoutAnimation.easeInEaseOut();
+                  setRecordingVideo(true);
+                  takeVideo();
+                } else {
+                  stopVideo();
+                  setRecordingVideo(false);
+                }
+              }}
+            />
+          </View>
+        </>
+      ) : takenPhoto && !takenVideo ? (
+        <>
+          <Image
+            source={{uri: takenPhoto}}
+            style={{width: '100%', height: '100%'}}
+          />
+        </>
+      ) : (
+        <View></View>
+      )}
     </View>
   );
 };
