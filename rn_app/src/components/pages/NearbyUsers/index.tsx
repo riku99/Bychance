8111e5ea3182d79ba6ef03562fcd2e626add20ae
riku <riku.suny@gmail.com>
2021-05-04 19:00:56 +0900

    fix name

--- rn_app/src/components/pages/NearbyUsers/index.tsx
+++ rn_app/src/components/pages/NearbyUsers/index.tsx
@@ -3,7 +3,7 @@ import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 import {useIsFocused} from '@react-navigation/native';
 import {useNavigation} from '@react-navigation/native';
 
-import {SearchUsers} from './SearchUsers';
+import {SearchUsers} from './nearbyUsersMain';
 import {RootState, AppDispatch} from '../../../stores/index';
 import {AnotherUser} from '../../../stores/types';
 import {selectNearbyUsersArray} from '../../../stores/nearbyUsers';
