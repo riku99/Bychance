f984ae5511484a92338c7e7adfe7184b10004a31
riku <riku.suny@gmail.com>
2021-06-07 14:51:01 +0900

    スタンプ表示の修正

--- rn_app/src/components/screens/Flashes/Stamps.tsx
+++ rn_app/src/components/screens/Flashes/Stamps.tsx
@@ -7,16 +7,19 @@ import {
   Dimensions,
   TextStyle,
 } from 'react-native';
+import {useSelector} from 'react-redux';
 
 import {createFlashStampThunk} from '~/apis/flashStamps/createFlashStamp';
 import {useCustomDispatch} from '~/hooks/stores/dispatch';
 import {Flash} from '~/stores/flashes';
+import {RootState} from '~/stores';
 
 type StampData = {
   label: string;
   value: string;
   number: number;
   style?: TextStyle;
+  disabled: boolean;
 };
 
 type Props = {
@@ -27,17 +30,22 @@ export const Stamps = React.memo(({flash}: Props) => {
   const {thumbsUp, yusyo, yoi, itibann, seikai} = useMemo(() => flash.stamps, [
     flash.stamps,
   ]);
+
+  const myId = useSelector((state: RootState) => state.userReducer.user!.id);
+
   const stampData: StampData[] = useMemo(() => {
     return [
       {
         label: '👍',
         value: 'thumbsUp',
         number: thumbsUp.number,
+        disabled: thumbsUp.userIds.includes(myId),
       },
       {
         label: '優勝',
         value: 'yusyo',
         number: yusyo.number,
+        disabled: yusyo.userIds.includes(myId),
         style: {
           fontFamily: 'Hiragino Sans',
           fontWeight: '700',
@@ -47,6 +55,7 @@ export const Stamps = React.memo(({flash}: Props) => {
         label: 'シンプルに\n良い',
         value: 'yoi',
         number: yoi.number,
+        disabled: yoi.userIds.includes(myId),
         style: {
           fontSize: 9.5,
           fontFamily: 'Hiragino Sans',
@@ -58,6 +67,7 @@ export const Stamps = React.memo(({flash}: Props) => {
         label: 'お前が\n1番',
         value: 'itibann',
         number: itibann.number,
+        disabled: itibann.userIds.includes(myId),
         style: {
           fontSize: 11,
           fontWeight: '700',
@@ -68,6 +78,7 @@ export const Stamps = React.memo(({flash}: Props) => {
         label: '見て正解',
         value: 'seikai',
         number: seikai.number,
+        disabled: seikai.userIds.includes(myId),
         style: {
           fontSize: 11,
           fontWeight: '700',
@@ -75,7 +86,7 @@ export const Stamps = React.memo(({flash}: Props) => {
         },
       },
     ];
-  }, [thumbsUp, yusyo, yoi, itibann, seikai]);
+  }, [thumbsUp, yusyo, yoi, itibann, seikai, myId]);
 
   const dispatch = useCustomDispatch();
 
@@ -91,9 +102,17 @@ export const Stamps = React.memo(({flash}: Props) => {
       {stampData.map((data) => {
         return (
           <TouchableOpacity
-            style={styles.stamp}
+            style={[
+              styles.stamp,
+              {
+                backgroundColor: data.disabled
+                  ? 'rgba(88,88,88,0.85)'
+                  : 'rgba(133,133,133,0.85)',
+              },
+            ]}
             key={data.label}
             activeOpacity={0.7}
+            disabled={data.disabled}
             onPress={() => createStamp({value: data.value})}>
             <Text style={[styles.stampText, {...data.style}]}>
               {data.label}
@@ -116,7 +135,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   stamp: {
-    backgroundColor: "backgroundColor: 'rgba(133,133,133,0.85)",
     width: width / 5 - 2.5,
     height: 35,
     borderRadius: 20,
