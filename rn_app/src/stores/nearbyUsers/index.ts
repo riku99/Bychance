667380dbd48f5a8025ddb5a2b7bc97f87cf1124c
riku <riku.suny@gmail.com>
2021-06-08 15:10:52 +0900

    いらん部分の削除とか

--- rn_app/src/stores/nearbyUsers/index.ts
+++ rn_app/src/stores/nearbyUsers/index.ts
@@ -16,10 +16,6 @@ import {
   CreateAlreadyViewdFlashThunkPayload,
 } from '../../apis/flashes/createAlreadyViewedFlashes';
 import {logoutThunk} from '~/apis/session/logout';
-import {
-  CreateFlashStampPayload,
-  createFlashStampThunk,
-} from '~/apis/flashStamps/createFlashStamp';
 
 // NearbyUserは位置情報により取得したユーザーなので必ずlat, lngが存在する。AnotherUserはトーク相手とかも含まれるので位置情報のデータが必ず含まれるとは限らない
 export type NearbyUser = Omit<AnotherUser, 'lat' | 'lng'> & {
@@ -55,47 +51,6 @@ const nearbyUsersSlice = createSlice({
       action: PayloadAction<CreateAlreadyViewdFlashThunkPayload>,
     ) => {
       updateAlreadyViewed(state, action, {slice: nearbyUsersSlice.name});
-    },
-    [createFlashStampThunk.fulfilled.type]: (
-      state,
-      action: PayloadAction<CreateFlashStampPayload>,
-    ) => {
-      const targetUser = state.entities[action.payload.ownerId];
-      if (targetUser) {
-        const targetEntites = targetUser.flashes.entities;
-        const targetFlash = targetEntites.find(
-          (data) => data.id === action.payload.flashId,
-        );
-        if (targetFlash) {
-          const targetStamp = targetFlash.stamps[action.payload.value];
-          const newTargetFlash = {
-            ...targetFlash,
-            stamps: {
-              ...targetFlash.stamps,
-              [action.payload.value]: {
-                number: targetStamp.number += 1,
-                userIds: [...targetStamp.userIds, action.payload.userId],
-              },
-            },
-          };
-          const newTargetEntities = targetEntites.map((data) => {
-            if (data.id === targetFlash.id) {
-              return newTargetFlash;
-            }
-
-            return data;
-          });
-          nearbyUsersAdapter.updateOne(state, {
-            id: targetUser.id,
-            changes: {
-              flashes: {
-                ...targetUser.flashes,
-                entities: newTargetEntities,
-              },
-            },
-          });
-        }
-      }
     },
   },
 });
