43e559ca209011ad1f760f0be72c89649194b6b1
riku <riku.suny@gmail.com>
2021-06-12 12:10:02 +0900

    説明の追加

--- rn_app/src/components/screens/UserConfig/DisplayConfig.tsx
+++ rn_app/src/components/screens/UserConfig/DisplayConfig.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useMemo, useState} from 'react';
-import {View, Switch} from 'react-native';
+import {View, Switch, Text} from 'react-native';
 import {useSelector} from 'react-redux';
 
 import {RootState} from '~/stores';
@@ -7,6 +7,7 @@ import {changeUserDisplayThunk} from '~/apis/users/changeUserDisplay';
 import {useCustomDispatch} from '~/hooks/stores';
 import {ConfigList} from './List';
 import {commonStyles} from './constants';
+import {CustomPopupModal} from '~/components/utils/PopupModal';
 
 export const DisplayConfig = React.memo(() => {
   const userDisplay = useSelector((state: RootState) => {
@@ -16,6 +17,10 @@ export const DisplayConfig = React.memo(() => {
   const dispatch = useCustomDispatch();
 
   const [switchForDisplay, setSwitchForDisplay] = useState(userDisplay);
+  const [
+    showDisplayDescriptionModal,
+    setShowDisplayDescriptionModal,
+  ] = useState(false);
 
   const onUserDisplaySwitchValueChange = useCallback(
     async (v: boolean) => {
@@ -39,6 +44,8 @@ export const DisplayConfig = React.memo(() => {
             onValueChange={onUserDisplaySwitchValueChange}
           />
         ),
+        description: true,
+        onItemPress: () => setShowDisplayDescriptionModal(true),
       },
     ];
   }, [switchForDisplay, onUserDisplaySwitchValueChange]);
@@ -48,6 +55,19 @@ export const DisplayConfig = React.memo(() => {
       <View style={commonStyles.sectionContainer}>
         <ConfigList list={list} />
       </View>
+      <CustomPopupModal
+        isVisible={showDisplayDescriptionModal}
+        closeModal={() => setShowDisplayDescriptionModal(false)}>
+        <View style={commonStyles.descriptionModal}>
+          <Text
+            style={{fontSize: 15, marginHorizontal: 10, marginVertical: 10}}>
+            ONの場合、一定の範囲内にいる他のユーザーに対して自分が表示されます。
+            {'\n'}OFFの場合は他のユーザーに表示されることはありません。
+            {'\n'}※
+            位置情報が削除される訳ではありません。削除したい場合はメニューの「位置情報」から行ってください。
+          </Text>
+        </View>
+      </CustomPopupModal>
     </View>
   );
 });
