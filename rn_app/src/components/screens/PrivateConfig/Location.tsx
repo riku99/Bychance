0caa35b42282519e40bdcfed53ba08e9d695b973
riku <riku.suny@gmail.com>
2021-06-13 19:53:54 +0900

    Locatinにいろいろ追加

--- rn_app/src/components/screens/PrivateConfig/Location.tsx
+++ rn_app/src/components/screens/PrivateConfig/Location.tsx
@@ -1,10 +1,15 @@
-import React, {useMemo} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Button} from 'react-native-elements';
-import MapView from 'react-native-maps';
+import {Button, Text} from 'react-native-elements';
+import MapView, {MapEvent} from 'react-native-maps';
 import {useSelector} from 'react-redux';
+import Geocoder from 'react-native-geocoding';
 
 import {RootState} from '~/stores';
+import {credentials} from '~/credentials';
+import {normalStyles} from '~/constants/styles';
+
+// Geocoder.init(credentials.GCP_API_KEY, {language: 'ja'});
 
 export const Location = React.memo(() => {
   const lat = useSelector((state: RootState) => state.userReducer.user!.lat);
@@ -21,6 +26,18 @@ export const Location = React.memo(() => {
     }
   }, [lat, lng]);
 
+  const [selectedAddress, setSelectedAdress] = useState('千葉県千葉市幕張1-1');
+  const [selectedCoordinate, setSelectedCoordinate] = useState<{
+    lat: number;
+    lng: number;
+  }>();
+
+  const onSelectMap = useCallback((e: MapEvent) => {
+    const {coordinate} = e.nativeEvent;
+    console.log(coordinate);
+    // アドレスの変換
+  }, []);
+
   return (
     <View style={styles.container}>
       <Button
@@ -33,12 +50,24 @@ export const Location = React.memo(() => {
         style={styles.map}
         region={region}
         showsUserLocation={true}
-        onPress={(e) => console.log(e.nativeEvent)}
+        onPress={onSelectMap}
       />
+      <View style={styles.selectedAddressContainer}>
+        <View style={{maxWidth: '65%'}}>
+          <Text style={styles.selectedAdressTitle}>選択された住所</Text>
+          <Text style={styles.selectedAdress}>{selectedAddress}</Text>
+        </View>
+        <Button
+          title="追加"
+          buttonStyle={styles.addButton}
+          titleStyle={styles.addButtonTitle}
+        />
+      </View>
     </View>
   );
 });
 
+const fontColor = '#4d4d4d';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -49,7 +78,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   descriptionButtonTitle: {
-    color: '#4d4d4d',
+    color: fontColor,
     fontSize: 15,
     fontWeight: '500',
     textDecorationLine: 'underline',
@@ -58,5 +87,33 @@ const styles = StyleSheet.create({
     height: '35%',
     width: '100%',
     marginTop: 10,
+  },
+  selectedAddressContainer: {
+    marginTop: 15,
+    width: '96%',
+    // backgroundColor: 'gray',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  selectedAdressTitle: {
+    color: fontColor,
+    fontWeight: '500',
+  },
+  selectedAdress: {
+    fontSize: 15,
+    marginTop: 10,
+    // backgroundColor: 'red',
+    maxWidth: '100%',
+  },
+  addButton: {
+    width: 55,
+    height: 30,
+    backgroundColor: normalStyles.mainColor,
+  },
+  addButtonTitle: {
+    fontSize: 15,
+    fontWeight: '500',
   },
 });
