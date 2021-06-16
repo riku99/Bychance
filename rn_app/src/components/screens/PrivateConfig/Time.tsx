38d4d2c0d26b03ef6438508107676e115f0e1342
riku <riku.suny@gmail.com>
2021-06-16 18:40:02 +0900

    レイアウトの追加

--- rn_app/src/components/screens/PrivateConfig/Time.tsx
+++ rn_app/src/components/screens/PrivateConfig/Time.tsx
@@ -1,10 +1,11 @@
 import React, {useCallback, useRef} from 'react';
-import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView, Dimensions} from 'react-native';
 import {Button, Divider} from 'react-native-elements';
 
 import {commonStyles} from './common';
 import {AboutPrivateTimeModal} from './AboutPrivateTimeModal';
 import {Modalize} from 'react-native-modalize';
+import {normalStyles} from '~/constants/styles';
 
 export const Time = React.memo(() => {
   const aboutPrivateTimeModalRef = useRef<Modalize>(null);
@@ -23,13 +24,80 @@ export const Time = React.memo(() => {
         activeOpacity={1}
         onPress={onAboutPrivateTimeButtonPress}
       />
+      <View style={styles.selectTimeContainer}>
+        <View style={styles.timeSection}>
+          <Text style={styles.sectionTitle}>開始</Text>
+          <Button
+            title="選択する"
+            buttonStyle={styles.selectButton}
+            titleStyle={styles.selectButotnTitle}
+            activeOpacity={1}
+          />
+        </View>
+        <Divider style={styles.divider} />
+        <View style={styles.timeSection}>
+          <Text style={styles.sectionTitle}>終了</Text>
+          <Button
+            title="選択する"
+            buttonStyle={styles.selectButton}
+            titleStyle={styles.selectButotnTitle}
+            activeOpacity={1}
+          />
+        </View>
+        <Button
+          buttonStyle={styles.addButton}
+          title="追加"
+          titleStyle={styles.addButotnTitleStyle}
+          activeOpacity={1}
+          disabled={true}
+        />
+      </View>
+      <SafeAreaView />
       <AboutPrivateTimeModal modalRef={aboutPrivateTimeModalRef} />
     </View>
   );
 });
 
+const {height} = Dimensions.get('screen');
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  selectTimeContainer: {
+    width: '100%',
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  timeSection: {
+    height: 70,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  sectionTitle: {
+    fontSize: 16,
+  },
+  divider: {
+    width: '100%',
+    alignSelf: 'center',
+  },
+  selectButton: {
+    backgroundColor: normalStyles.mainColor,
+    height: 30,
+  },
+  selectButotnTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  addButton: {
+    width: 80,
+    marginTop: 15,
+    backgroundColor: normalStyles.mainColor,
+  },
+  addButotnTitleStyle: {
+    fontSize: 14,
+    fontWeight: '500',
   },
 });
