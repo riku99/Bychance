36301c17995a4a7476d9c4c838c595c75fb7ddac
riku <riku.suny@gmail.com>
2021-06-18 18:06:07 +0900

    プライベートタイムのモーダルの内容修正

--- rn_app/src/components/screens/PrivateConfig/AboutPrivateTimeModal.tsx
+++ rn_app/src/components/screens/PrivateConfig/AboutPrivateTimeModal.tsx
@@ -13,7 +13,18 @@ export const AboutPrivateTimeModal = React.memo(({modalRef}: Props) => {
     <Modalize ref={modalRef} modalHeight={height * 0.8}>
       <View style={commonModalStyles.modalContainer}>
         <Text style={commonModalStyles.title}>プライベートタイムとは</Text>
-        <Text style={commonModalStyles.explanation}>hello</Text>
+        <Text style={commonModalStyles.explanation}>
+          他のユーザーに対して自動で自分を表示させなくする時間のことです。
+          {'\n'}
+          {'\n'}
+          プライベートタイムに設定した時間内である場合、例え設定で「他のユーザーに自分を表示」をオンにしていても他のユーザーに自分が表示されなくなります。🙅‍♀️
+          {'\n'}
+          {'\n'}
+          プライベートタイムは複数登録することができます。🔢
+          {'\n'}
+          {'\n'}
+          プライベートタイム中でも、他のユーザーに自分を表示させたくない場合は設定で「他のユーザーに自分を表示」をオフにすることを推奨します。🙇‍♂️
+        </Text>
       </View>
     </Modalize>
   );
