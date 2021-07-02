3c1cfcbbb1d5918991bcad38cb03e49ea5de18d5
riku <riku.suny@gmail.com>
2021-07-02 23:35:28 +0900

    logoの追加

--- rn_app/src/components/screens/Auth/Auth.tsx
+++ rn_app/src/components/screens/Auth/Auth.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Image, View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Button} from 'react-native-elements';
+import Logo from '~/assets/logo.svg';
 
 const lineBase = require('../../../assets/btn_login_base.png');
 const linePress = require('../../../assets/btn_login_press.png');
@@ -14,6 +15,12 @@ export const Auth = ({login, sampleLogin}: Props) => {
   const [pressLoginButton, setPressLoginButton] = useState(false);
   return (
     <View style={styles.container}>
+      <Logo
+        onLayout={(e) => console.log(e.nativeEvent)}
+        height={100}
+        width="75%"
+        style={{marginTop: '45%', alignSelf: 'center'}}
+      />
       <TouchableOpacity
         activeOpacity={1}
         style={styles.loginButtonContainer}
