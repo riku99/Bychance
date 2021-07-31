c75c51193387ec66e1661d872cf9a7fc5c17f981
riku <riku.suny@gmail.com>
2021-07-31 19:32:29 +0900

    fix toast

--- rn_app/src/App.tsx
+++ rn_app/src/App.tsx
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {store} from './stores/index';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {ToastProvider} from 'react-native-fast-toast';
+import MIcon from 'react-native-vector-icons/MaterialIcons';
 
 import Root from './components/Root';
 import {normalStyles} from '~/constants/styles';
@@ -16,6 +17,8 @@ const App: () => React.ReactNode = () => {
         <ToastProvider
           placement="bottom"
           offset={toastOffset}
+          successIcon={<MIcon name="done" color="white" size={17} />}
+          dangerIcon={<MIcon name="clear" color="white" size={17} />}
           style={{width: bottomToastWidth}}>
           <Root />
         </ToastProvider>
