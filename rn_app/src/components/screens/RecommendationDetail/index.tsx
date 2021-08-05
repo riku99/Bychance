a10a698851d5af2901b4401f96658c7fbc59e0cb
riku <riku.suny@gmail.com>
2021-08-05 11:44:48 +0900

    add Button

--- rn_app/src/components/screens/RecommendationDetail/index.tsx
+++ rn_app/src/components/screens/RecommendationDetail/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import {RecommendationDetail as _RecommendationDetail} from 'bychance-components';
 import {useRoute, RouteProp} from '@react-navigation/native';
+import {Button} from 'react-native-elements';
 
 import {RecommendationStackParamList} from '~/navigations/Recommendation';
 
@@ -10,7 +11,18 @@ export const RecommendationDetail = React.memo(() => {
 
   return (
     <View style={styles.container}>
-      <_RecommendationDetail data={data.params} />
+      <_RecommendationDetail
+        data={data.params}
+        BottomButton={() => (
+          <Button
+            title="非表示にする"
+            activeOpacity={1}
+            titleStyle={{fontWeight: 'bold'}}
+            containerStyle={styles.buttonContainer}
+            buttonStyle={styles.button}
+          />
+        )}
+      />
     </View>
   );
 });
@@ -19,5 +31,15 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
+  },
+  buttonContainer: {
+    marginTop: 40,
+    width: '90%',
+    alignSelf: 'center',
+    borderRadius: 50,
+  },
+  button: {
+    backgroundColor: 'gray',
+    height: 41,
   },
 });
