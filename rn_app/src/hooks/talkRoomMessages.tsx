43593151cf9d97caa3d53a96a4be34e5c16ab917
riku <riku.suny@gmail.com>
2021-08-14 13:22:07 +0900

    fix socket

--- rn_app/src/hooks/talkRoomMessages.tsx
+++ rn_app/src/hooks/talkRoomMessages.tsx
@@ -146,18 +146,11 @@ export const useSetupTalkRoomMessageSocket = () => {
   const [socket, setSocket] = useState<Socket>();
   const dispatch = useCustomDispatch();
 
-  if (socket) {
-    socket.on('connect', () => {
-      console.log('cnne');
-    });
-  }
-
   useEffect(() => {
     // active時に毎回サブスクリプションする
     const onActive = (nextAppState: AppStateStatus) => {
       if (nextAppState === 'active') {
         if (id && !socket) {
-          console.log('sw');
           setSocket(io(`${_origin}/talkRoomMessages`, {query: {id}}));
         }
       } else {
@@ -174,4 +167,30 @@ export const useSetupTalkRoomMessageSocket = () => {
       AppState.removeEventListener('change', onActive);
     };
   }, [id, dispatch, socket]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('recieveTalkRoomMessage', (data: ReceivedMessageData) => {
+        dispatch(receiveTalkRoomMessage(data));
+        if (AppState.currentState === 'active' && data.show) {
+          showMessage({
+            message: data.sender.name,
+            description: data.message.text,
+            style: {backgroundColor: '#00163b'},
+            titleStyle: {color: 'white', marginLeft: 10},
+            textStyle: {color: 'white', marginLeft: 10},
+            icon: 'default',
+            duration: 2000,
+            renderFlashMessageIcon: () => {
+              return (
+                <View style={{marginRight: 5}}>
+                  <UserAvatar size={40} image={data.sender.avatar} />
+                </View>
+              );
+            },
+          });
+        }
+      });
+    }
+  }, [socket, dispatch]);
 };
