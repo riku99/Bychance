db45606d1968d7c2f685b1bdfe3c3a58e09dee37
riku <riku.suny@gmail.com>
2021-08-26 14:49:22 +0900

    dipatch posts

--- rn_app/src/hooks/users.ts
+++ rn_app/src/hooks/users.ts
@@ -5,8 +5,6 @@ import {default as axios} from 'axios';
 import useSWR from 'swr';
 
 import {RootState} from '~/stores/index';
-import {UserPageFrom} from '~/navigations/UserPage';
-import {selectNearbyUser} from '~/stores/nearbyUsers';
 import {useApikit} from './apikit';
 import {User} from '~/types/users';
 import {baseUrl} from '~/constants/url';
@@ -33,61 +31,6 @@ export const useSelectTamporarilySavedUserEditData = () => {
 
 export const useMyId = () =>
   useSelector((state: RootState) => state.userReducer.user!.id);
-
-export const useAnotherUser = ({
-  from,
-  userId,
-}: {
-  from?: UserPageFrom;
-  userId?: string;
-}) =>
-  useSelector((state: RootState) => {
-    if (from && userId) {
-      switch (from) {
-        case 'nearbyUsers':
-          return selectNearbyUser(state, userId);
-        case 'chatRoom':
-        // return selectChatPartner(state, userId);
-      }
-    }
-  }, shallowEqual);
-
-// ユーザーページのように自分のデータか他のユーザーのデータが必要となる時に使う
-export const useUser = ({
-  from,
-  userId,
-}: {
-  from?: UserPageFrom;
-  userId?: string;
-}) => {
-  const myId = useMyId();
-  // userIdが存在しない(Tabから呼び出された)またはuserIdがmyIdと同じ(stackから自分のデータを渡して呼び出した)場合はtrue
-  const isMe = !userId || myId === userId;
-
-  const me = useSelector((state: RootState) => {
-    if (isMe) {
-      return state.userReducer.user!;
-    }
-  }, shallowEqual);
-
-  const anotherUser = useAnotherUser({from, userId});
-
-  // meとanotherUserで共通して使えるものについてはわざわざmeであるかanotherUserであるか検証したくないのでuserとしてまとめる
-  // 別々のものとして使いたい時はme, anotherUserのどちらかを使う
-  if (isMe) {
-    return {
-      user: me,
-      me,
-      isMe,
-    };
-  } else {
-    return {
-      user: anotherUser,
-      anotherUser,
-      isMe,
-    };
-  }
-};
 
 type EditArg = {
   name: string;
@@ -421,6 +364,7 @@ export const useUserPageInfo = (userId: string) => {
         };
       });
 
+      dispatch(upsertPosts(response.data.posts));
       dispatch(upsertFlashes(storedFlashesData));
       dispatch(
         upsertUsers([
