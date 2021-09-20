64f755c47c94f8a686cb510efcdba439e18f4c86
riku <riku.suny@gmail.com>
2021-09-20 14:02:03 +0900

    申請されたときのメッセージ表示

--- rn_app/src/hooks/applyingGroups.ts
+++ rn_app/src/hooks/applyingGroups.ts
@@ -1,6 +1,8 @@
 import {useCallback, useEffect, useState} from 'react';
+import {AppState, AppStateStatus} from 'react-native';
 import io, {Socket} from 'socket.io-client';
 import {default as axios} from 'axios';
+import {showMessage} from 'react-native-flash-message';
 
 import {useMyId} from './users';
 import {origin, baseUrl} from '~/constants/url';
@@ -14,6 +16,7 @@ export const useSetupApplyingGroupSocket = () => {
   const id = useMyId();
   const [socket, setSocket] = useState<Socket>();
 
+  // 初回レンダリングではonChangeが実行されないのでここでサブスクリプション
   useEffect(() => {
     if (id) {
       setSocket(io(`${origin}/applying_group`, {query: {id}}));
@@ -29,21 +32,51 @@ export const useSetupApplyingGroupSocket = () => {
     }
   }, [id, socket]);
 
+  // アクティブ時に毎回サブスクリプション。それ以外は切断。
+  useEffect(() => {
+    const onChange = (nextAppEvent: AppStateStatus) => {
+      if (nextAppEvent === 'active') {
+        if (id && !socket) {
+          setSocket(io(`${origin}/applying_group`, {query: {id}}));
+        }
+      } else {
+        if (socket) {
+          // @ts-ignore
+          socket.removeAllListeners();
+          socket.disconnect();
+          setSocket(undefined);
+        }
+      }
+    };
+
+    AppState.addEventListener('change', onChange);
+
+    return () => {
+      AppState.removeEventListener('change', onChange);
+    };
+  }, [id, socket]);
+
   useEffect(() => {
     if (socket) {
       socket.on('applyGroup', (data) => {
-        console.log(data);
+        showMessage({
+          message: 'グループ申請を受け取りました',
+          description: '',
+          duration: 2000,
+          style: {backgroundColor: '#00163b'},
+          titleStyle: {fontWeight: 'bold'},
+        });
       });
     }
   }, [socket]);
 
-  // useEffect(() => {
-  //   if (socket) {
-  //     socket?.on('connect', () => {
-  //       console.log('connectt');
-  //     });
-  //   }
-  // }, [socket]);
+  useEffect(() => {
+    if (socket) {
+      socket?.on('connect', () => {
+        console.log('connectt');
+      });
+    }
+  }, [socket]);
 };
 
 export const useCreateApplyingGroup = () => {
