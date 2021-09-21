ed692964bee5d6a7dcc0cdd2ceb79fd7b8c90805
riku <riku.suny@gmail.com>
2021-09-21 11:43:37 +0900

    badge更新

--- rn_app/src/components/screens/UserConfig/GroupConfig.tsx
+++ rn_app/src/components/screens/UserConfig/GroupConfig.tsx
@@ -4,18 +4,25 @@ import {View, StyleSheet, Switch} from 'react-native';
 import {ConfigList} from './List';
 import {commonStyles} from './constants';
 import {useGroupsApplicationEnabled} from '~/hooks/settings';
+import {useGroupBadge} from '~/hooks/appState';
 
 const GroupsApplicationEnabledSwitch = () => {
   const {
     groupsApplicationEnabled,
     changeGroupsApplicationEnabled,
   } = useGroupsApplicationEnabled();
+  const {setGroupBadge} = useGroupBadge();
   const [switchValue, setSwitchValue] = useState(groupsApplicationEnabled);
   const onValueChange = async (v: boolean) => {
     setSwitchValue(v);
     const result = await changeGroupsApplicationEnabled(v);
+
     if (!result) {
       setSwitchValue(!v);
+    }
+
+    if (result && !v) {
+      setGroupBadge(false);
     }
   };
 
