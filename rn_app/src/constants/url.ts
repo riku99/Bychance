ce2cb400a30ec2beecdb9e51e74dad79083e7717
riku <riku.suny@gmail.com>
2021-11-23 18:44:31 +0900

    fix BI

--- rn_app/src/constants/url.ts
+++ rn_app/src/constants/url.ts
@@ -2,6 +2,6 @@
 
 // export const origin =
 //   'http://bychance-api-server-stg-alb-577685766.ap-northeast-1.elb.amazonaws.com';
-export const origin = 'http://localhost:4001';
+export const origin = 'https://bychance-api-stg-ubt27vytva-an.a.run.app';
 
 export const baseUrl = `${origin}/api/v1`;
