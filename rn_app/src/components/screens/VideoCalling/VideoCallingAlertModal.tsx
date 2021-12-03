adb91728aa6e8a82d814b6858681d902bd2287de
riku <riku.suny@gmail.com>
2021-12-03 16:27:14 +0900

    add VideoCallingAlertModal

--- rn_app/src/components/screens/VideoCalling/VideoCallingAlertModal.tsx
+++ rn_app/src/components/screens/VideoCalling/VideoCallingAlertModal.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 import Modal from 'react-native-modal';
 import {useVideoCallingAlertModalVisible} from '~/hooks/appState';
 import {ModalCloseButton} from '~/components/utils/ModalCloseButton';
+import LottieView from 'lottie-react-native';
+
+const Alert = require('~/assets/lottie/alert.json');
 
 type Props = {
   isVisible: boolean;
@@ -19,6 +22,25 @@ export const VideoCallingAlertModal = () => {
             setV(false);
           }}
         />
+        <LottieView source={Alert} autoPlay style={styles.alert} loop={false} />
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>
+            相手ユーザーが設定で
+            <Text style={styles.textBold}>「ビデオ通話を受け取る」</Text>
+            をOFFにしている場合、
+            <Text style={styles.textUnderColor}>
+              このビデオ通話は相手に知らされず繋がりません。
+            </Text>
+            {'\n'}
+            {'\n'}
+            相手の設定がONになっているかOFFになっているかを知ることはできません。
+            {'\n'}
+            {'\n'}
+            <Text style={styles.textSub}>
+              ※ 今後このお知らせは表示されません
+            </Text>
+          </Text>
+        </View>
       </View>
     </Modal>
   );
@@ -32,10 +54,35 @@ const styles = StyleSheet.create({
   },
   contents: {
     width: '90%',
-    height: 450,
+    height: 320,
     backgroundColor: 'white',
   },
   closeButton: {
+    position: 'absolute',
     transform: [{translateX: -11}, {translateY: -11}],
+  },
+  alert: {
+    width: 120,
+    height: 120,
+    alignSelf: 'center',
+  },
+  textContainer: {
+    marginTop: 10,
+    paddingHorizontal: 12,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+  },
+  textBold: {
+    fontWeight: 'bold',
+  },
+  textUnderColor: {
+    textDecorationLine: 'underline',
+    textDecorationColor: '#f54242',
+  },
+  textSub: {
+    color: '#242424',
+    fontSize: 14,
   },
 });
